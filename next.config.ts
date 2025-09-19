db433ce8f1af7548b31f46ce2c2cb16d92fa67a6
Lakshya Gupta <130373277+CodeWithLakshya@users.noreply.github.com>
2025-09-19 17:49:10 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  output: "export", // generates static HTML for GitHub Pages
+  images: {
+    unoptimized: true, // disable Next.js image optimization
+  },
+  basePath: "/Manex-The-Smartest-Way-to-Manage-and-Grow", // important for GitHub Pages
+  assetPrefix: "/Manex-The-Smartest-Way-to-Manage-and-Grow/",
   /* config options here */
   webpack: (config) => {
     config.resolve.alias = {
